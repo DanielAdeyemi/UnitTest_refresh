97d3f223267ba8dd4fbeb865ff99619d64291555
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 19:46:17 -0800

    use different methods for assert.that

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -13,6 +13,10 @@ namespace SparkyNUnitTest
             var customer = new Customer();
             string fullName = customer.GreetAndCombineNames("Ben", "Spark");
             Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+            Assert.That(fullName, Does.Contain("ben Spark").IgnoreCase);
+            Assert.That(fullName, Does.StartWith("Hello,"));
+            Assert.That(fullName, Does.EndWith("Spark"));
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
         }
     }
 }
