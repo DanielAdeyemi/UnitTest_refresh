b55d1caf97087250ae83a1c198b2d79bc14bebd3
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 21:18:58 -0800

    add one more class in calc

--- Sparky/Calculator.cs
+++ Sparky/Calculator.cs
@@ -1,7 +1,9 @@
-﻿namespace Sparky
+﻿using System.Collections.Generic;
+namespace Sparky
 {
     public class Calculator
     {
+        public List<int> NumberRange = new();
         public int AddNums(int a, int b)
         {
             return a + b;
@@ -14,6 +16,19 @@
         public bool isOddNum(int a)
         {
             return a % 2 != 0;
+        }
+
+        public List<int> GetOddRange(int min, int max)
+        {
+            NumberRange.Clear();
+            for(int i = min; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    NumberRange.Add(i);
+                }
+            }
+            return NumberRange;
         }
     }
 }
