8683284e5ab1e99ba23967edb405b6f8e99cff1d
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 19:38:20 -0800

    write tests for string

---
+++ Sparky/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Sparky
+{
+	public class Customer
+	{
+		public string GreetAndCombineNames(string firstName, string lastName)
+		{
+			return $"Hello, {firstName} {lastName}";
+		}
+	}
+}
+
---
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineName_InputNames_ReturnGreeting()
+        {
+            var customer = new Customer();
+            string fullName = customer.GreetAndCombineNames("Ben", "Spark");
+            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+        }
+    }
+}
+
