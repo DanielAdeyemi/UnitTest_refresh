796a92863e7c4523644a4a9efad37c1eb1ac2ec4
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 21:30:59 -0800

    add test for check - failed

--- Sparky/Customer.cs
+++ Sparky/Customer.cs
@@ -9,6 +9,6 @@ namespace Sparky
 			GreetMessage = $"Hello, {firstName} {lastName}";
             return GreetMessage;
 		}
-	}
+    }
 }
 
--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 namespace Sparky
 {
 	[TestFixture]
 	public class CalculatorNUnitTests
-	{
+    {
+		private Calculator calc;
+		[SetUp]
+		public void Setup()
+		{
+			calc = new Calculator();
+		}
+
 		[Test]
 		public void AddNums_InputTwoInt_GetCorrectAddition()
 		{
-			Calculator calc = new();
 			int result = calc.AddNums(10, 20);
 			Assert.AreEqual(30, result);
 		}
+
 		[Test]
 		public void IsOddNums_InputEven_GetFalse()
         {
-			Calculator calc = new();
 			bool result = calc.isOddNum(2);
             Assert.IsFalse(result);
         }
+
 		[Test]
 		[TestCase(11)]
 		[TestCase(13)]
 		public void IsOddNums_InputOdd_GetTrue(int a)
         {
-			Calculator calc = new();
             bool result = calc.isOddNum(a);
             Assert.IsTrue(result);
         }
@@ -35,19 +42,26 @@ namespace Sparky
 
 		public bool IsOddNums_InputNum_GetBoolResult(int a)
         {
-			Calculator calc = new();
 			return calc.isOddNum(a);
         }
+
 		[Test]
 		[TestCase(5.4, 10.5)]
 		[TestCase(5.43, 10.53)]
 		[TestCase(5.49, 10.59)]
 		public void AddNumsDouble_InputTwoDoubles_GetCorrectAddition(double a, double b)
         {
-			Calculator calc = new();
 			double result = calc.AddDoubles(a, b);
-			Assert.AreEqual(15.9, result, 1);
+            Assert.AreEqual(15.9, result, 1);
         }
-	}
+
+        [Test]
+        public void OddRange_InputMinAndMaxRange_ReturnValidNumsRange()
+        {
+			List<int> expectedOddRange = new() { 5, 7, 9 };
+			List<int> result = calc.GetOddRange(3, 10);
+			Assert.That(result, Is.EquivalentTo(expectedOddRange));
+        }
+    }
 }
 
