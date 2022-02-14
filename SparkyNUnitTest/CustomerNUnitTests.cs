b79d2a1a1a89da4a2faa097306c7823f48281006
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 21:50:41 -0800

    add assert multiple

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -17,12 +17,16 @@ namespace SparkyNUnitTest
         [Test]
         public void CombineName_InputNames_ReturnGreeting()
         {
-            customer.GreetAndCombineNames("Ben", "Spark");
-            Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
-            Assert.That(customer.GreetMessage, Does.Contain("ben Spark").IgnoreCase);
-            Assert.That(customer.GreetMessage, Does.StartWith("Hello,"));
-            Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
-            Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
+            Assert.Multiple(() =>
+            {
+
+                customer.GreetAndCombineNames("Ben", "Spark");
+                Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
+                Assert.That(customer.GreetMessage, Does.Contain("ben Spark").IgnoreCase);
+                Assert.That(customer.GreetMessage, Does.StartWith("Hello,"));
+                Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
+                Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
+            });
         }
 
         [Test]
