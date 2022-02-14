448216fdb369ebc331da5bbfb62638c47664ab13
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 17:39:32 -0800

    unit test practice

---
+++ Sparky/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparky
+{
+    public class Calculator
+    {
+        public int AddNums(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+
+namespace SparkyMSTest
+{
+	[TestClass]
+	public class CalculatorTests
+	{
+		[TestMethod]
+		public void AddNums_InputTwoInt_GetCorrectAddition()
+		{
+			Calculator calc = new();
+			int result = calc.AddNums(10, 20);
+			Assert.AreEqual(30, result);
+		}
+	}
+}
+
