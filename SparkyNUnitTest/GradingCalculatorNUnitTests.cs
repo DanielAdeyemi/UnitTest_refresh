f3fac06a11b7af9247ba0cd31aceee16a6d31d48
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 23:36:46 -0800

    combine all cases in one test

--- SparkyNUnitTest/GradingCalculatorNUnitTests.cs
+++ SparkyNUnitTest/GradingCalculatorNUnitTests.cs
@@ -52,6 +52,21 @@ namespace SparkyNUnitTest
                 calc.AttendancePercentage = b;
                 Assert.AreEqual("F", calc.GetGrade());
             }
+
+            [Test]
+            [TestCase(95, 90, ExpectedResult = "A")]
+            [TestCase(85, 90, ExpectedResult = "B")]
+            [TestCase(95, 65, ExpectedResult = "B")]
+            [TestCase(65, 90, ExpectedResult = "C")]
+            [TestCase(95, 55, ExpectedResult = "F")]
+            [TestCase(65, 55, ExpectedResult = "F")]
+            [TestCase(50, 90, ExpectedResult = "F")]
+            public string GradingCalculator_ScoreAndAttendance_Grade(int score, int attendance)
+            {
+                calc.Score = score;
+                calc.AttendancePercentage = attendance;
+                return calc.GetGrade();
+            }
         }
     }
 }
