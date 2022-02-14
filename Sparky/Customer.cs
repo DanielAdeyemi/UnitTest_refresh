fcaf15e8c6f0da93fbab6b0dd1c77ad24ed15f22
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 19:58:38 -0800

    refactor test with setup

--- Sparky/Customer.cs
+++ Sparky/Customer.cs
@@ -3,9 +3,11 @@ namespace Sparky
 {
 	public class Customer
 	{
+		public string GreetMessage { get; set; }
 		public string GreetAndCombineNames(string firstName, string lastName)
 		{
-			return $"Hello, {firstName} {lastName}";
+			GreetMessage = $"Hello, {firstName} {lastName}";
+            return GreetMessage;
 		}
 	}
 }
--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -7,16 +7,22 @@ namespace SparkyNUnitTest
     [TestFixture]
     public class CustomerNUnitTests
     {
+        private Customer customer;
+        [SetUp]
+        public void Setup()
+        {
+            customer = new Customer();
+        }
+
         [Test]
         public void CombineName_InputNames_ReturnGreeting()
         {
-            var customer = new Customer();
-            string fullName = customer.GreetAndCombineNames("Ben", "Spark");
-            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
-            Assert.That(fullName, Does.Contain("ben Spark").IgnoreCase);
-            Assert.That(fullName, Does.StartWith("Hello,"));
-            Assert.That(fullName, Does.EndWith("Spark"));
-            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
+            customer.GreetAndCombineNames("Ben", "Spark");
+            Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
+            Assert.That(customer.GreetMessage, Does.Contain("ben Spark").IgnoreCase);
+            Assert.That(customer.GreetMessage, Does.StartWith("Hello,"));
+            Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
+            Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
         }
     }
 }
