cb74fdabec45885934e9212963ca6af4e6869240
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 23:06:52 -0800

    add test for A and setup global gc obj

---
+++ SparkyNUnitTest/GradingCalculatorNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+	public class GradingCalculatorNUnitTests
+	{
+        [TestFixture]
+        public class CustomerNUnitTests
+        {
+            private GradingCalculator calc;
+            [SetUp]
+            public void Setup()
+            {
+                calc = new GradingCalculator();
+            }
+
+            [Test]
+            public void GradingCalculator_Attendance90AndScore95_GradeA()
+            {
+                calc.AttendancePercentage = 90;
+                calc.Score = 95;
+                Assert.AreEqual("A", calc.GetGrade());
+            }
+        }
+    }
+}
+
