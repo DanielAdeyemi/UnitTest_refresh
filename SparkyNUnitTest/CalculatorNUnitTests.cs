3bd096d2684369bcbd2fedbd950e6e40ab6816d1
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-13 18:01:09 -0800

    add nunit test and fail it

---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+namespace Sparky
+{
+	[TestFixture]
+	public class CalculatorNUnitTests
+	{
+		[Test]
+		public void AddNums_InputTwoInt_GetCorrectAddition()
+		{
+			Calculator calc = new();
+			int result = calc.AddNums(10, 20);
+			Assert.AreEqual(10, result);
+		}
+	}
+}
+
