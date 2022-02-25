aebc3a299ac46ed35413574e95c0230db3a7e1d5
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-24 21:42:35 -0800

    add test for getting balance

---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+	[TestFixture]
+	public class BankAccountNUnitTests
+	{
+		private BankAccount bankAccount;
+		[SetUp]
+		public void Setup ()
+        {
+			bankAccount = new(new LogBook());
+        }
+		[Test]
+		public void BankDeposit_Add100_ReturnTrue()
+        {
+			var result = bankAccount.Deposit(0);
+			Assert.IsTrue(result);
+			Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }
+	}
+}
+
