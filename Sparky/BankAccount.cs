5b6b5d3b1c01e8db5db2905db1448419800d5ae5
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-24 16:32:18 -0800

    add bank account class

---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sparky
+{
+	public class BankAccount
+	{
+		public int balance { get; set; }
+		public BankAccount()
+		{
+			balance = 0;
+		}
+		public bool Deposit(int amount)
+		{
+			balance += amount;
+			return true;
+		}
+
+		public bool Withdraw(int amount)
+		{
+			if (amount <= balance)
+			{
+				balance -= amount;
+				return true;
+			}
+			return false;
+		}
+
+		public int GetBalance()
+        {
+			return balance;
+        }
+    }
+}
+
+
