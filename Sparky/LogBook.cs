31bf9304c032d9836b2dede70f4386fc51c97c7e
DanielAdeyemi <adeyemidany@gmail.com>
2022-02-24 21:33:43 -0800

    add logbook

---
+++ Sparky/LogBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Sparky
+{
+	public interface ILogBook
+    {
+		void Message(string message);
+    }
+
+	public class LogBook : ILogBook
+	{
+		public void Message(string message)
+        {
+			Console.WriteLine(message);
+        }
+	}
+}
+
